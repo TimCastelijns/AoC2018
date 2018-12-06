f31096137344d9cc02f96cf36737199f57e868f2
Tim Castelijns <tim.castelijns@mediamonks.com>
2018-12-06 14:32:39 +0100

    Polish a bit

--- src/day6/6.kt
+++ src/day6/6.kt
@@ -3,22 +3,22 @@ package day6
 import java.io.File
 import kotlin.system.measureTimeMillis
 
-val input = File("src/day6/input").readLines()
-val chars = (IntRange(65, 90) + IntRange(97, 122)).map { it.toChar() }
+private val input = File("src/day6/input").readLines()
+private val chars = (IntRange(65, 90) + IntRange(97, 122)).map { it.toChar() }
 
 private val coordinates = input.mapIndexed { i, line ->
     val parts = line.split(", ")
     Coordinate(chars[i], parts[0].toInt(), parts[1].toInt())
 }
 
-val x = coordinates.sortedByDescending { it.x }[0].x + 1
-val y = coordinates.sortedByDescending { it.y }[0].y + 1
+private val x = coordinates.sortedByDescending { it.x }[0].x + 1
+private val y = coordinates.sortedByDescending { it.y }[0].y + 1
 
 // Calculate which x/y define the boundaries.
-val top = coordinates.minBy { it.y }!!.y
-val bottom = coordinates.maxBy { it.y }!!.y
-val left = coordinates.minBy { it.x }!!.x
-val right = coordinates.maxBy { it.x }!!.x
+private val top = coordinates.minBy { it.y }!!.y
+private val bottom = coordinates.maxBy { it.y }!!.y
+private val left = coordinates.minBy { it.x }!!.x
+private val right = coordinates.maxBy { it.x }!!.x
 
 fun main(args: Array<String>) {
     var time = measureTimeMillis {
@@ -69,11 +69,6 @@ private fun p1(): Int {
 private fun p2(): Int {
     val grid = Array(x) { Array(y) { '-' } }
 
-    // Put coordinate names on grid.
-    coordinates.forEach {
-        grid[it.x][it.y] = it.name
-    }
-
     for (i in 0 until x) {
         for (j in 0 until y) {
             val location = Coordinate('#', i, j)
@@ -81,15 +76,15 @@ private fun p2(): Int {
                 location.manhattanDistance(it)
             }
             if (manhattanDistanceSum < 10000) {
-                grid[i][j] = location.name
+                grid[location.x][location.y] = location.name
             }
         }
     }
 
-    return grid.flatten().count { it == '#'}
+    return grid.flatten().count { it == '#' }
 }
 
-fun Array<Array<Char>>.print() {
+private fun Array<Array<Char>>.print() {
     forEach {
         it.forEach { n ->
             print("$n ")
@@ -97,7 +92,6 @@ fun Array<Array<Char>>.print() {
         println()
     }
 }
-
 
 private fun Map.Entry<Coordinate, Int>.isFiniteArea(grid: Array<Array<Char>>): Boolean {
     // For each coordinate we must check if they don't stretch out to one of the boundaries.
@@ -113,4 +107,4 @@ private fun Map.Entry<Coordinate, Int>.isFiniteArea(grid: Array<Array<Char>>): B
 private fun Coordinate.manhattanDistance(other: Coordinate) =
     Math.abs(this.x - other.x) + Math.abs(this.y - other.y)
 
-data class Coordinate(val name: Char, val x: Int, val y: Int)
+private data class Coordinate(val name: Char, val x: Int, val y: Int)
